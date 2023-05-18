c24a4f39e528ba386e8d3ac8f228c048a8841354
wallace <wallace_wolv@hotmail.com>
2023-05-18 13:08:26 -0300

    feat: Criando Blur Background e Stripes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
-
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import {
-  useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
+  useFonts,
 } from '@expo-google-fonts/roboto'
+import { StatusBar } from 'expo-status-bar'
+import { ImageBackground } from 'react-native'
 
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -21,9 +25,13 @@ export default function App() {
   }
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="font-title text-5xl text-gray-50">Rocketseat</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
