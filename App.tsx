f5e7a2597ded0e407b10eced8a27673a845bb0af
wallace <wallace_wolv@hotmail.com>
2023-05-18 13:21:13 -0300

    feat: Seção Hero

--- App.tsx
+++ App.tsx
@@ -5,11 +5,12 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto'
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground } from 'react-native'
+import { styled } from 'nativewind'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
 import blurBg from './src/assets/bg-blur.png'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
 import Stripes from './src/assets/stripes.svg'
-import { styled } from 'nativewind'
 
 const StyledStripes = styled(Stripes)
 
@@ -27,10 +28,34 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center bg-gray-900"
+      className="relative flex-1 items-center bg-gray-900 px-8"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6 ">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="light" translucent />
     </ImageBackground>
   )
